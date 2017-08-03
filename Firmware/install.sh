d90d9d0a051d3a00d5a157c1c69ace1a716c95e8
Divyang Solanki <divyangsolanki16@gmail.com>
2017-08-03 17:48:56 +0530

    Update install.sh

--- Firmware/install.sh
+++ Firmware/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "pip html-linter installing start"
+echo "pip djangorestframework installing start"
 pip install --user djangorestframework==3.6.3
+pip install --user geoip2==0.4.0
 echo "completed"
 
