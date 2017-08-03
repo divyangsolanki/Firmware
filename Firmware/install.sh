b2827465f8725ef2cac36cf9c921f26b17883373
Divyang Solanki <divyangsolanki16@gmail.com>
2017-08-03 17:51:23 +0530

    Update install.sh

--- Firmware/install.sh
+++ Firmware/install.sh
@@ -3,5 +3,7 @@
 echo "pip djangorestframework installing start"
 pip install --user djangorestframework==3.6.3
 pip install --user geoip2==0.4.0
+pip install --user flask==0.12.2
+pip install --user bottle==0.12.13
 echo "completed"
 
